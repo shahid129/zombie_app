7ed54e7f8c26b28c6ff7d5569b2ac7a690e2a5ea
Shahidul Islam <shahid.okay@gmail.com>
2023-05-21 21:34:01 +0100

    Added dependencies and other static files

--- zombie_app/src/App.jsx
+++ zombie_app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+        <h1>Hellow world</h1>
+    </div>
+)
 
 export default App
