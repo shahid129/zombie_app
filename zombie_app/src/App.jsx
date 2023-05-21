4e9d37456542ccc2d069cebdd39c3b124088cabc
Shahidul Islam <shahid.okay@gmail.com>
2023-05-21 23:10:09 +0100

    Added content to navbar

--- zombie_app/src/App.jsx
+++ zombie_app/src/App.jsx
@@ -1,21 +1,45 @@
-import styles from './style'
+import styles from './style';
+import './index.css';
+import {
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Navbar,
+    Stats,
+    Testimonials,
+    Hero,
+} from './components';
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-            <div className={`${styles.boxWidth}`}>Navbar</div>
+            <div className={`${styles.boxWidth}`}>
+                <Navbar />
+            </div>
         </div>
 
         <div className={`bg-primary ${styles.flexStart}`}>
-            <div className={`${styles.boxWidth}`}>Hero</div>
+            <div className={`${styles.boxWidth}`}>
+                <Hero />
+            </div>
         </div>
 
-        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
             <div className={`${styles.boxWidth}`}>
-                Stats Business Billing Card deal testi cleients CTA Footer
+                <Stats />
+                <Business />
+                <Billing />
+                <CardDeal />
+                <Testimonials />
+                <Clients />
+                <CTA />
+                <Footer />
             </div>
         </div>
     </div>
-)
+);
 
-export default App
+export default App;
